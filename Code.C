61d14b05fc408d4bec1d978453ae2f7a37051fba
Slayne <slaynemoriarty@gmail.com>
2025-01-14 14:16:19 +0100

    Update Code.C

--- Code.C
+++ Code.C
@@ -8,3 +8,19 @@ typedef struct{
       int numero;
       char email[50];
   }contact;
+
+
+//Fonction pour charger les contactes depuis un fichier binaire
+
+void chargerContact(contact p[100], int cont){
+    FILE *fichier=fopen("C:\\Users\\Tiptop\\Documents\\contact.txt","rb");
+
+    if(fichier==NULL){
+        printf("Impossible de charger le contact\n");
+    }
+
+    fread(&cont,sizeof(int),1,fichier);
+    fread(p,sizeof(contact),cont,fichier);
+    fclose(fichier);
+    printf("Chargement effectue\n");
+}
